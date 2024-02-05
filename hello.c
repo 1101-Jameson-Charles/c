2a8d8c36df7e853f6b912f380f170401e727f623
ian.jameson <prodigalson3257@gmail.com>
2024-02-04 22:38:01 -0800

    init repo c +hello +hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+//Name: hello.c
+//Purpose: Hello World Program!
+//Author: Ian Jameson
+
+#include <stdio.h>
+int main (){
+	
+	printf("Hello There!\n");
+	printf("My name is Ian Jameson, and I'm happy to be coding\n");
+	printf("These are my first steps\n");
+	return 0;
+}
